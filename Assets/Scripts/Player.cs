533419685e840291110b8fa494e7e8353886f54f
Miniieee <bagyi.tomi93@gmail.com>
2023-03-09 17:47:31 +0000

    Projectile prototype
    selecting the closest enemy does not work yet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private PlayerMovement playerMovement;
     [SerializeField] private float playerSpeed;
+    [SerializeField] private Projectile projectile;
 
 
     /*TODO
@@ -20,11 +21,21 @@ public class Player : MonoBehaviour
 
     private void Start() {
         playerMovement = GetComponent<PlayerMovement>();
+        StartCoroutine(SpawnProjectile());
     }
 
     private void Update() {
         Vector2 movementVectorNormalized = playerMovement.GetInputVectorNormalized();
         Vector3 direction = new Vector3(movementVectorNormalized.x, movementVectorNormalized.y, 0f);
         transform.position += direction * Time.deltaTime * playerSpeed;
+    }
+
+    //prototype functions
+    IEnumerator SpawnProjectile(){
+        while (true)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed = 1f;
-    private Transform[] selectedEnemies;
+    private Enemy[] selectedEnemies;
+    private float distance;
+    private float distanceFromEnemy;
+    private float currentDistance;
+    private Enemy selectedEnemy;
 
     
     void Start()
     {
-        
+        distance = Mathf.Infinity;
+        selectedEnemies = FindObjectsOfType<Enemy>();
+        foreach (Enemy _enemy in selectedEnemies)
+        {
+            currentDistance = Vector3.Distance(transform.position, _enemy.transform.position);
+            if (currentDistance < distance)
+            {
+                selectedEnemy = _enemy;
+            }
+        }
     }
 
     
     void Update()
     {
-        
+        Vector3 dir = selectedEnemy.transform.position - transform.position;
+        transform.position += dir * projectileSpeed * Time.deltaTime;
+        distanceFromEnemy = Vector3.Distance(transform.position, selectedEnemy.transform.position);
+
+        if (distanceFromEnemy < Mathf.Epsilon)
+        {
+            Debug.Log("damaged by " + selectedEnemy.name);
+        }
     }
 }
